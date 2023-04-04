d34f1858db936840dbbb002d06666ca37739cd1b
Matthew Schroeter <schroeter.m83@gmail.com>
2023-04-04 15:46:56 -0500

    possibly fixed issue with theme and plugins not activating on initial install

--- post-install.sh
+++ post-install.sh
@@ -7,7 +7,7 @@ RESET='\033[0m' # No Color
 # Yarn Install and compile assets
 function compile_assets(){
     cd ./wp-content/themes/$site_slug/ || exit
-    nvm use --lts
+    [[ -z nvm ]] && nvm use --lts
     composer install
     yarn && yarn build
     cd -
@@ -39,6 +39,9 @@ function wp_setup(){
     wp core download --path=wp --skip-content --force --allow-root
     wp db create --path=wp
     wp core install --url="http://${PWD##*/}.loc" --title="$site_name" --admin_name="$admin_name" --admin_password="$admin_password" --admin_email="test@test.com" --path=wp
+
+    compile_assets
+
     wp theme activate $site_slug --path=wp
 }
 
