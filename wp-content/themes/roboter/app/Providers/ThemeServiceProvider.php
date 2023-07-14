cd9c1d0d5a2deb280629030d895efe42fd697073
Matthew Schroeter <schroeter.m83@gmail.com>
2023-07-14 12:44:37 -0500

    added daisy nav menu class integration to ThemeServiceProviders

--- wp-content/themes/roboter/app/Providers/ThemeServiceProvider.php
+++ wp-content/themes/roboter/app/Providers/ThemeServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Roots\Acorn\Sage\Sage;
 use Roots\Acorn\Sage\SageServiceProvider;
 
 class ThemeServiceProvider extends SageServiceProvider
@@ -14,6 +15,7 @@ class ThemeServiceProvider extends SageServiceProvider
     public function register()
     {
         parent::register();
+
     }
 
     /**
@@ -24,5 +26,41 @@ class ThemeServiceProvider extends SageServiceProvider
     public function boot()
     {
         parent::boot();
+
+        $this->bindDaisyNavClasses();
+    }
+
+    public function bindDaisyNavClasses() : void {
+        add_filter('nav_menu_submenu_css_class', [self::class, 'navSubmenuClasses'], 99, 3);
+        add_filter('nav_menu_css_class', [self::class, 'navMenuItemClasses'], 99, 4);
+        add_filter('nav_menu_item_args', [self::class, 'navMenuItemArgs'], 99, 3);
+        add_filter('nav_menu_link_attributes', [self::class, 'navMenuLinkAttributes'], 99, 4);
+    }
+
+    public static function navMenuItemClasses(array $classes, \WP_Post $menu_item, \stdClass $args, int $depth) : array {
+        if($depth === 0) {
+            $classes[] = 'hover:bg-darkerBlue';
+        }
+        return $classes;
+    }
+
+    public static function navMenuItemArgs(\stdClass $args, \WP_Post $item,  int $depth) : object {
+        return $args;
+    }
+
+    public static function navMenuLinkAttributes(array $atts, \WP_Post $menu_item, \stdClass $args, int $depth) : array {
+        $atts['class'] = 'hover:text-blue';
+        return $atts;
+    }
+
+    public static function navSubmenuClasses(array $classes, \stdClass $args, int $depth) : array {
+        $classes[] = 'menu';
+        $classes[] = 'p-4';
+        $classes[] = 'w-full';
+        $classes[] = 'bg-clip-content';
+        $classes[] = 'bg-primaryWhite';
+        $classes[] = '!text-primaryBlack';
+
+        return $classes;
     }
 }
